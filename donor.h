cc490438835ac572fa062690e0694a0639534d3b
farzanaislam-del <ahornikaislam@gmail.com>
2026-08-03 01:20:40 +0600

    Add files via upload

---
+++ donor.h
@@ -0,0 +1,16 @@
+#ifndef DONOR_H
+#define DONOR_H
+
+typedef struct
+{
+    int id;
+    char name[50];
+    int age;
+    char bloodGroup[5];
+} Donor;
+
+void addDonor();
+void displayDonors();
+void searchDonor();
+
+#endif
